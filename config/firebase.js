868cf1036643fc4e6ed1de8c2874d7b8afa2af58
Mahesh <81896665+mahesh863@users.noreply.github.com>
2021-10-15 11:52:16 +0530

    Delete config directory

--- config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseConfig.json");
-require("dotenv").config();
-
-module.exports = {
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_URL,
-};
